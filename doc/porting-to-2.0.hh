3bef0780bf53eaed05f236ffa3f30fa43dc83497
Florent Lamiraux <florent@laas.fr>
2011-07-20 21:42:17 +0200

    Write porting notes.

--- doc/porting-to-2.0.hh
+++ doc/porting-to-2.0.hh
@@ -9,6 +9,17 @@
 ///
 /// \section hpp_model_porting_modification Modification in client code
 ///
+/// \subsection hpp_model_porting_header_renaming Header renaming
+///
+/// \li header <c>hppModel/hppDevice.h</c> has been replaced by <c>hpp/model/device.hh</c>.
+/// \li header <c>hppModel/hppBody.h</c> has been replaced by <c>hpp/model/body.hh</c>
+/// \li header <c>hppModel/hppHumanoidRobot.h</c> has been replaced by <c>hpp/model/humanoid-robot.hh</c>
+/// \li header <c>hppModel/hppJoint.h</c> has been replaced by <c>hpp/model/joint.hh</c>
+/// \li header <c>hppModel/hppImplRobotDynamics.h</c> has been replaced by <c>hpp/model/robot-dynamics-impl.hh</c>
+/// \li header <c>hppModel/hppSpecificHumanoidRobot.h</c> has been replaced by <c>hpp/model/specific-humanoid-robot.hh</c>
+///
+/// \subsection hpp_model_porting_class_renaming Class renaming
+///
 /// \li ChppDevice has been renamed into hpp::model::Device
 /// \li ChppHumanoidRobot has been renamed into hpp::model::HumanoidRobot
 /// \li ChppJoint has been renamed into hpp::model::Joint
