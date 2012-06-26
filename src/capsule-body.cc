4d85b41462b436f468c1b5c3a8ff74e890edc091
Antonio El Khoury <aelkhour@laas.fr>
2012-06-26 13:54:47 +0200

    Temporary fix, do not check whether object can be cast to solid component.

--- src/capsule-body.cc
+++ src/capsule-body.cc
@@ -140,9 +140,9 @@ namespace hpp {
 	CkppSolidComponentShPtr solidComponent
 	  = KIT_DYNAMIC_PTR_CAST (CkppSolidComponent, innerCapsule);
 
-	if (solidComponent) {
-	  hppDout(info,"adding " << solidComponent->name ()
-		  << " to list of capsules for distance computation.");
+	// if (solidComponent) {
+	//   hppDout(info,"adding " << solidComponent->name ()
+	// 	  << " to list of capsules for distance computation.");
 	  innerCapsulesForDist_.push_back (innerCapsule);
 	  // Build Exact distance computation pairs for capsule
 	  const std::vector<capsule_t>& outerList = outerCapsulesForDist_;
@@ -159,11 +159,11 @@ namespace hpp {
 		    << outerName);
 	    capsuleDistCompPairs_.push_back(distCompPair);
 	  }
-	}
-	else {
-	  hppDout(error,"cannot cast solid component into CkcdObject.");
-	  throw Exception("cannot cast solid component into CkcdObject.");
-	}
+	// }
+	// else {
+	//   hppDout(error,"cannot cast solid component into CkcdObject.");
+	//   throw Exception("cannot cast solid component into CkcdObject.");
+	// }
       }
       return true;
     }
