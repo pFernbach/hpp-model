bbe82a3c8a259f1cd0b3e7bb9630fd7aeea73b61
Florent Lamiraux <florent@laas.fr>
2014-11-08 09:20:47 +0100

    Cosmetic change
      Cut long lines.

--- src/joint-configuration.cc
+++ src/joint-configuration.cc
@@ -373,8 +373,10 @@ namespace hpp {
         value_type cosq = q [indexConfig];
         value_type sinq = q [indexConfig + 1];
 	value_type norm2p = q.segment (indexConfig, 2).squaredNorm ();
-	result [indexConfig    ] = (1.5-.5*norm2p) * (cosOmega * cosq - sinOmega * sinq);
-	result [indexConfig + 1] = (1.5-.5*norm2p) * (sinOmega * cosq + cosOmega * sinq);
+	result [indexConfig    ] = (1.5-.5*norm2p) *
+	  (cosOmega * cosq - sinOmega * sinq);
+	result [indexConfig + 1] = (1.5-.5*norm2p) *
+	  (sinOmega * cosq + cosOmega * sinq);
       }
 
       void UnBounded::difference (ConfigurationIn_t q1, ConfigurationIn_t q2,
